f30d09ba3d5d41fe0986bd2f94608c2873066f67
Raihan Arvi <raihanadhiarvi@gmail.com>
2025-07-11 00:24:32 -0700

    initial file

---
+++ main.py
@@ -0,0 +1,13 @@
+from gui.gui import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+app = QApplication([])
+window = Window()
+
+window.show()
+app.exec()
